fcd7f6297c56f708ef5a85232b9e3a39239fce34
Rishab <rishabagrawal@hotmail.com>
2019-04-18 16:46:25 -0700

    added js and flickity

---
+++ js folder/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    // All of your code goes inside here 
+    console.log('this is working');
+
+
+
+
+      const flkty = new Flickity( '.main-carousel', {
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true,
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ });// end of document ready 
